0f8686a6e0fef73058fff885187239c06923ce9a
eliana osorio <yasminosorio2013@gmail.com>
2023-10-24 13:57:25 -0500

    feat: proyect start

---
+++ react-hello/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function HelloWorld(params) {
+    return(
+        <div>Hola Mundo</div>
+    )
+}
+
+ReactDOM.render(
+
+    <HelloWorld/>>
+)
