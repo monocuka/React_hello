70e296925347bf434b6f5d768b637e48f3310523
eliana osorio <yasminosorio2013@gmail.com>
2023-11-08 16:49:50 -0500

    feat: refactoring functions

--- src/tweet/buttons.js
+++ src/tweet/buttons.js
@@ -2,38 +2,34 @@ import React from "react";
 
 export const ReplyButton = () => (<i className="fa fa-reply reply-button" />); 
 
-export const RetweetButton = ({count}) => 
-    (
-        <span className="retweet-button">
-            <i className="fa fa-retweet" />
-            {getRetweetCount(count)}
-        </span>
-    );
-
-const  getRetweetCount = (count) => {
-
-    if(count >0){
+const Count = ({ count }) => {
+    if (count > 0) {
         return (
-            <span className="retweet-count">
-                {count}
+            <span className="retweet-count" >
+            {count}
             </span>
-        )
-    }else{
-        return null;
+        );
+    } else {
+        return null ;
     }
-}    
+}
 
-export const LikeButton=({count}) => (
+export const RetweetButton = ({ count }) => (
+    <span className="retweet-button" >
+    <i className="fa fa-retweet" />
+    <Count count={count} />
+    </span>
+);
 
-    <span className="like-button">
-        <i className="fa fa-heart" /> 
-        {count >0 &&
-            <span className="like-count">
-                {count}
-            </span>}
+export const LikeButton = ({ count }) => (
+    <span className="like-button" >
+        <i className="fa fa-heart" />
+        <span className="like-count" >
+        {count ? count : null }
+        </span>
     </span>
 );
 
 export const MoreOptionsButton=() => (
     <i className="fa fa-ellipsis-h more-options-button" />
-);
+); 
